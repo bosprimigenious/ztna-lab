b8412c8542b4e96a6c3313e8bb52268cbadc405d
bosprimigenious <bosprimigenious@foxmail.com>
2026-09-16 13:21:26 +0800

    Add certificate key usage extensions

--- labztna/certs.py
+++ labztna/certs.py
@@ -71,6 +71,20 @@ def _issue_server_certificate(
             critical=False,
         )
         .add_extension(
+            x509.KeyUsage(
+                digital_signature=True,
+                content_commitment=False,
+                key_encipherment=True,
+                data_encipherment=False,
+                key_agreement=False,
+                key_cert_sign=False,
+                crl_sign=False,
+                encipher_only=False,
+                decipher_only=False,
+            ),
+            critical=True,
+        )
+        .add_extension(
             x509.ExtendedKeyUsage([ExtendedKeyUsageOID.SERVER_AUTH]), critical=False
         )
         .sign(ca_key, hashes.SHA256())
@@ -102,6 +116,20 @@ def create_ephemeral_bundle(directory: str | Path) -> TLSBundle:
         .not_valid_before(now - timedelta(minutes=1))
         .not_valid_after(now + timedelta(days=1))
         .add_extension(x509.BasicConstraints(ca=True, path_length=0), critical=True)
+        .add_extension(
+            x509.KeyUsage(
+                digital_signature=False,
+                content_commitment=False,
+                key_encipherment=False,
+                data_encipherment=False,
+                key_agreement=False,
+                key_cert_sign=True,
+                crl_sign=True,
+                encipher_only=False,
+                decipher_only=False,
+            ),
+            critical=True,
+        )
         .add_extension(
             x509.SubjectKeyIdentifier.from_public_key(ca_key.public_key()),
             critical=False,
